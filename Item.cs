da805e87b6a203bb73c393297920b9855d29ba64
Ruslan <theyneedr@gmail.com>
2020-10-25 23:28:28 +0200

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace HashTable
+{
+    class Item<T>
+    {
+        public int Key { get; set; }
+        public List<T> Nodes { get; set; }
+
+        public Item(int key)
+        {
+            Nodes = new List<T>();
+            Key = key;
+        }
+    }
+}
